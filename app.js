72a9fc56938a3b6db915c93b9ebb027ae7c38906
Ishan Aryan <toxic.valorant.yt@gmail.com>
2023-09-14 23:46:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+let nav = document.querySelector("nav");
+let lines = document.querySelector(".lines");
+lines.addEventListener("click", () => {
+  nav.classList.toggle("classforheader");
+});
+
+let navlistli = document.querySelectorAll(".navlist li");
+navlistli.forEach((item, index) => {
+  setTimeout(() => {
+    item.style.transform = "translateY(0%)";
+  }, index * 100);
+});
+
+const addClassWhenNear = (selector, className, distance) => {
+  const element = document.querySelector(selector);
+  if (!element) return;
+  window.addEventListener("scroll", () => {
+    const rect = element.getBoundingClientRect();
+    if (rect.top - distance < window.innerHeight) {
+      element.classList.add(className);
+      window.removeEventListener("scroll", addClassWhenNear);
+    }
+  });
+};
+
+addClassWhenNear(".ms", "showup", 5);
+addClassWhenNear(".sr", "showup", 5);
+addClassWhenNear(".um", "showup", 5);
+addClassWhenNear(".ow", "showup", 5);
+
+let logo = document.querySelector(".logo");
+logo.style.transform = "translateY(0%)";
+
+
